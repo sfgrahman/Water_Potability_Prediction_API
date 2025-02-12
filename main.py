832f6c2a70f6efaeb882fd284f05f875d93268a8
Moududur Shamim <sfgrahman35@gmail.com>
2025-02-12 15:58:16 +0600

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI(
     description = "Predicting Water Potability"
 )
 
-with open(r"C:\Users\sfg\mlops_practice\Water Quality and Potability_ML_Pipeline\model.pkl", "rb") as f:
+with open(r"model.pkl", "rb") as f:
     model = pickle.load(f)
     
 @app.get("/")
